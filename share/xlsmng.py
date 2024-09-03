ef6db8e39f3ec811afae4166c6e0271ca447d70f
Alex Occelli <occelli.alex@gmail.com>
2024-09-03 22:08:03 +0200

    xlsmng v1.0.1 - aggiunto docstrings

--- share/xlsmng.py
+++ share/xlsmng.py
@@ -13,7 +13,15 @@ FORMATS = {
 }
 
 
-def write_excel(fou: str, rows: list, sheet_name: str = None, header: list[str] = None, font_face: str = None) -> None:
+def write_excel(fou: str, rows: list, sheet_name: str = None, header: list[str] = None, font_face: str = DEFAULT_FONT) -> None:
+    """Write a list of values into Excel file.
+
+    :param str fou: Path to the result file.
+    :param list rows: List of values to write.
+    :param str sheet_name: Name of the sheet into the file, defaults to None.
+    :param list[str] header: List of column names, defaults to None.
+    :param str font_face: Font face of the file, defaults to DEFAULT_FONT constant.
+    """
     wb = openpyxl.Workbook()
     ws = wb.active
     if sheet_name:
@@ -22,13 +30,13 @@ def write_excel(fou: str, rows: list, sheet_name: str = None, header: list[str] 
     if header:
         for col_num, name in enumerate(header, start=1):
             ws.cell(row=1, column=col_num).value = name
-            ws.cell(row=1, column=col_num).font = Font(name=font_face if font_face else DEFAULT_FONT, bold=True)
+            ws.cell(row=1, column=col_num).font = Font(name=font_face, bold=True)
             ws.cell(row=1, column=col_num).number_format = FORMATS[type(name)]
 
     for row_num, row in enumerate(rows, start=2 if header else 1):
         for col_num, col in enumerate(row, start=1):
             ws.cell(row=row_num, column=col_num).value = col
-            ws.cell(row=row_num, column=col_num).font = Font(name=font_face if font_face else DEFAULT_FONT)
+            ws.cell(row=row_num, column=col_num).font = Font(name=font_face)
             ws.cell(row=row_num, column=col_num).number_format = FORMATS[type(col)]
 
     if header:
