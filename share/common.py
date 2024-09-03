862905b3192ddf11763529a92d432495c81396b9
Alex Occelli <occelli.alex@gmail.com>
2024-09-03 22:05:10 +0200

    common v1.2.1 - aggiunto docstrings

--- share/common.py
+++ share/common.py
@@ -3,6 +3,13 @@ import logging
 
 
 def decode_json(json_fin: str, key: str, value: str) -> list | None:
+    """Read a json file and return the objects which verify the condition {key: value}.
+
+    :param str json_fin: Path to the json file.
+    :param str key: The key name to be verified.
+    :param str value: The value to look for.
+    :return: A list of matching objects in the file.
+    """
     with open(json_fin, 'r') as jin:
         res = list(json.load(jin))
 
@@ -10,7 +17,14 @@ def decode_json(json_fin: str, key: str, value: str) -> list | None:
     return res if res else None
 
 
-def logger_ini(fou: str, name: str = 'main', log_level: int = logging.INFO) -> logging.Logger:
+def logger_ini(fou: str, name: str = 'main', log_level: int | str = logging.INFO) -> logging.Logger:
+    """Initialize a new logger object with custom properties.
+
+    :param str fou: Path to the log file.
+    :param str name: Name of the logger, defaults to 'main'.
+    :param int or str log_level: Logging level, defaults to INFO.
+    :return: The logger, created if it doesn't exist.
+    """
     logger = logging.getLogger(name)
     if not len(logger.handlers):
         logger.setLevel(log_level)
