4636e6adf4989c86110f6372ac5c700471b3db77
Alex Occelli <occelli.alex@gmail.com>
2024-09-03 21:56:41 +0200

    common v1.2.0 - bug fixing in logger_ini

--- share/common.py
+++ share/common.py
@@ -10,15 +10,16 @@ def decode_json(json_fin: str, key: str, value: str) -> list | None:
     return res if res else None
 
 
-def logger_ini(fou: str, log_level: int = logging.INFO) -> logging.Logger:
-    logger = logging.getLogger(__name__)
-    logger.setLevel(log_level)
+def logger_ini(fou: str, name: str = 'main', log_level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not len(logger.handlers):
+        logger.setLevel(log_level)
 
-    handler = logging.FileHandler(fou)
-    handler.setFormatter(logging.Formatter(
-        '%(asctime)s [%(name)s] %(levelname)s - %(message)s',
-        '%d/%m/%Y %H:%M:%S'
-    ))
+        handler = logging.FileHandler(fou)
+        handler.setFormatter(logging.Formatter(
+            '%(asctime)s [%(name)s] %(levelname)s - %(message)s',
+            '%d/%m/%Y %H:%M:%S'
+        ))
 
-    logger.addHandler(handler)
+        logger.addHandler(handler)
     return logger
