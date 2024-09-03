fcf0a0bced356bf1cc07715f372132bdcfcf13af
Alex Occelli <occelli.alex@gmail.com>
2024-09-03 21:52:55 +0200

    recording_doc v2.0.0 - refactoring usando pypdfium2

--- src/recording_doc.py
+++ src/recording_doc.py
@@ -1,9 +1,8 @@
 from datetime import date
 from share import sqlmng
 from share.common import logger_ini
-import pdfplumber
-import PyPDF2
 import os
+import pypdfium2
 import re
 
 PATH_PRJ = 'c:/source/vanaheim'
@@ -13,10 +12,11 @@ PATH_DISCARDED = f'{PATH_WORKING_DIR}/discarded'
 PATH_RECORDED = f'{PATH_WORKING_DIR}/recorded'
 
 PATTERN_WORKING_DOC = r'^\d{4}_\d{2}_DDT_\d{4}_\d{4}\.pdf$'
-PATTERN_NUMERO_DATA = r'Num\. D\.D\.T\. (.+) Data D\.D\.T\. (\d{2}/\d{2}/\d{4}) Pag\.'
-PATTERN_ASTRA_SERVIZI = r'Luogo di consegna\nASTRA SERVIZI SOC.COOP.\nVIA DELLA MOTORIZZAZIONE'
-PATTERN_QUANTITA = r'Qta: (.+),000'
-PATTERN_TARGA = r'Targa automezzo (.{7})'
+PATTERN_NUMERO_DATA = r'Num\. D\.D\.T\. ([\d\.]+)\/(\w{2}) Data D\.D\.T\. (\d{2}\/\d{2}\/\d{4}) Pag'
+PATTERN_SEDE_SX = r"Luogo di partenza: .+\r\n([\w\s\.\&\-']+)\r\n(\d{5}) ([\w\s']+) \(?(\w{2})\)?\r\n"
+PATTERN_SEDE_DX = r"Luogo di consegna\r\n([\w\s\.\&\-']+)\r\n.+\r\n(\d{0,5}) ?([\w\s']+) \(?(\w{2})\)?\r\n"
+PATTERN_QUANTITA = r'(Quantità Prezzo\r\n.+)? (L|KG) ([\d\.]+),000\r\n'
+PATTERN_TARGA = r'Peso soggetto accisa\r\n([\w\d]{7})\r\n'
 
 QUERY_CHK_DUPLICATE = """
     SELECT COUNT(*)
@@ -30,7 +30,6 @@ QUERY_CHK_DUPLICATE = """
         AND YEAR(data_documento) = YEAR(?)
     );
 """
-
 QUERY_INSERT = """
     INSERT INTO consegne (
         numero_documento,
@@ -48,94 +47,102 @@ QUERY_INSERT = """
 
 
 def doc_scanner(working_doc: str) -> tuple[int, int]:
-    with pdfplumber.open(working_doc) as doc:
-        discarded_pages = 0
+    doc = pypdfium2.PdfDocument(working_doc)
+    discarded_pages = 0
 
-        for page in doc.pages:
-            working_page = page.page_number
-            logger.info(f'scanning on page {working_page} of {working_doc}...')
-            text = page.extract_text()
+    for working_page, page in enumerate(doc, start=1):
+        logger.info(f'scanning on page {working_page} of {working_doc}...')
+        text = page.get_textpage().get_text_bounded()
 
-            doc_info = {}
-            search = re.search(PATTERN_NUMERO_DATA, text)
+        doc_info = {}
+        search = re.search(PATTERN_NUMERO_DATA, text)
+        if search:
+            doc_info['numero_documento'] = int(search.group(1).replace('.', ''))
+            doc_info['genere_documento'] = search.group(1)
+            doc_info['data_documento'] = search.group(2)[6:] + '-' + search.group(2)[3:5] + '-' + search.group(2)[0:2]
+        else:
+            discarded_pages += 1
+            discarded_doc = discard_doc(working_doc, working_page)
+            logger.warning(f'discarding page {working_page} of {working_doc} for error on PATTERN_NUMERO_DATA... [{discarded_doc}]')
+            # TODO: save message on DB
+            continue
+
+        search = re.search(PATTERN_SEDE_DX, text)
+        # TODO: check sede_consegna in DB comuni
+        if search:
+            doc_info['ragione_sociale'] = search.group(1)
+            doc_info['sede_consegna'] = search.group(3)
+        else:
+            search = re.search(PATTERN_SEDE_SX, text)
             if search:
-                doc_info['numero_documento'] = int(search.group(1).replace('.', '').split('/')[0])
-                doc_info['genere_documento'] = search.group(1).split('/')[1]
-                doc_info['data_documento'] = search.group(2)[6:] + '-' + search.group(2)[3:5] + '-' + search.group(2)[0:2]
+                doc_info['ragione_sociale'] = search.group(1)
+                doc_info['sede_consegna'] = search.group(3)
             else:
                 discarded_pages += 1
                 discarded_doc = discard_doc(working_doc, working_page)
-                logger.warning(f'discarding page {working_page} of {working_doc} for error on PATTERN_NUMERO_DATA... [{discarded_doc}]')
+                logger.warning(f'discarding page {working_page} of {working_doc} for error on PATTERN_SEDE... [{discarded_doc}]')
                 # TODO: save message on DB
                 continue
 
-            search = re.search(PATTERN_ASTRA_SERVIZI, text)
-            doc_info['ragione_sociale'] = 'ASTRA SERVIZI SOC. COOP.' if search else None
-            doc_info['sede_consegna'] = 'CUNEO' if search else None
+        search = re.search(PATTERN_QUANTITA, text)
+        if search:
+            doc_info['quantita'] = int(search.group(3).replace('.', ''))
+        else:
+            discarded_pages += 1
+            discarded_doc = discard_doc(working_doc, working_page)
+            logger.warning(f'discarding page {working_page} of {working_doc} for error on PATTERN_QUANTITA... [{discarded_doc}]')
+            # TODO: save message on DB
+            continue
 
-            search = re.search(PATTERN_QUANTITA, text)
-            if search:
-                doc_info['quantita'] = int(search.group(1).replace('.', ''))
-            else:
-                discarded_pages += 1
-                discarded_doc = discard_doc(working_doc, working_page)
-                logger.warning(f'discarding page {working_page} of {working_doc} for error on PATTERN_QUANTITA... [{discarded_doc}]')
-                # TODO: save message on DB
-                continue
+        doc_info['data_consegna'] = doc_info['data_documento']
 
-            doc_info['data_consegna'] = doc_info['data_documento']
+        search = re.search(PATTERN_TARGA, text)
+        if search:
+            doc_info['targa'] = search.group(1)
+        else:
+            discarded_pages += 1
+            discarded_doc = discard_doc(working_doc, working_page)
+            logger.warning(f'discarding page {working_page} of {working_doc} for error on PATTERN_TARGA... [{discarded_doc}]')
+            # TODO: save message on DB
+            continue
 
-            search = re.search(PATTERN_TARGA, text)
-            if search:
-                doc_info['targa'] = search.group(1)
-            else:
-                discarded_pages += 1
-                discarded_doc = discard_doc(working_doc, working_page)
-                logger.warning(f'discarding page {working_page} of {working_doc} for error on PATTERN_TARGA... [{discarded_doc}]')
-                # TODO: save message on DB
-                continue
+        doc_info['sorgente'] = working_doc.replace('.recording', '')
+        doc_info['pagina'] = working_page
+        logger.debug(doc_info)
 
-            doc_info['sorgente'] = working_doc.replace('.recording', '')
-            doc_info['pagina'] = working_page
-            logger.debug(doc_info)
+        cursor = sqlmng.conx_ini(save_changes=True)
+        chk_dup = sqlmng.conx_read(cursor, QUERY_CHK_DUPLICATE, (
+            doc_info['sorgente'],
+            doc_info['pagina'],
+            doc_info['numero_documento'],
+            doc_info['genere_documento'],
+            doc_info['data_documento']
+        )).fetchone()[0]
 
-            cursor = sqlmng.conx_ini(save_changes=True)
-            chk_dup = sqlmng.conx_read(cursor, QUERY_CHK_DUPLICATE, (
-                doc_info['sorgente'],
-                doc_info['pagina'],
-                doc_info['numero_documento'],
-                doc_info['genere_documento'],
-                doc_info['data_documento']
-            )).fetchone()[0]
+        if chk_dup != 0:
+            discarded_pages += 1
+            discarded_doc = discard_doc(working_doc, working_page)
+            logger.warning(f'discarding page {working_page} of {working_doc} because already recorded... [{discarded_doc}]')
+            # TODO: save message on DB
+            continue
+        elif sqlmng.conx_write(cursor, QUERY_INSERT, doc_info) != 1:
+            discarded_pages += 1
+            discarded_doc = discard_doc(working_doc, working_page)
+            logger.error(f'discarding page {working_page} of {working_doc} for error on saving record... [{discarded_doc}]')
+            # TODO: save message on DB
+            continue
 
-            if chk_dup != 0:
-                discarded_pages += 1
-                discarded_doc = discard_doc(working_doc, working_page)
-                logger.warning(f'discarding page {working_page} of {working_doc} because already recorded... [{discarded_doc}]')
-                # TODO: save message on DB
-                continue
-            elif sqlmng.conx_write(cursor, QUERY_INSERT, doc_info) != 1:
-                discarded_pages += 1
-                discarded_doc = discard_doc(working_doc, working_page)
-                logger.error(f'discarding page {working_page} of {working_doc} for error on saving record... [{discarded_doc}]')
-                # TODO: save message on DB
-                continue
-
-        return len(doc.pages), discarded_pages
+    return len(doc), discarded_pages
 
 
 def discard_doc(working_doc: str, working_page: int) -> str:
-    doc = PyPDF2.PdfReader(working_doc)
-    discard = PyPDF2.PdfWriter()
+    doc = pypdfium2.PdfDocument(working_doc)
+    discard = pypdfium2.PdfDocument().new()
 
-    discard.add_page(doc.pages[working_page - 1])
-    with open(f"{PATH_DISCARDED}/{working_doc.split('.')[0]}_P{working_page:0>3}.pdf", 'wb') as fou:
-        discard.write(fou)
-    return fou.name.split('/')[-1]
-
-
-def save_message(message: str) -> None:
-    pass
+    discard.import_pages(doc, [working_page - 1])
+    discard_doc_name = f"{working_doc.split('.')[0]}_P{working_page:0>3}.pdf"
+    discard.save(f'{PATH_DISCARDED}/{discard_doc_name}')
+    return discard_doc_name
 
 
 if __name__ == '__main__':
