ca0e67091567dd6ca251e2b19e543c30431a214d
Alex Occelli <occelli.alex@gmail.com>
2024-09-03 22:20:49 +0200

    overview_doc v1.1.0 - rimosso hard code in overview_gnr

--- src/overview_doc.py
+++ src/overview_doc.py
@@ -23,13 +23,43 @@ QUERY_OVERVIEW_DATA = """
         AND EXTRACT(MONTH FROM data_consegna) = ?
     ORDER BY numero_documento;
 """
-QUERY_TRAVELS_SUMMARY = """
-    SELECT data_consegna, sede_consegna 
-    FROM vanaheim.consegne
-    WHERE EXTRACT(YEAR FROM data_consegna) = ?
-        AND targa = ?
-    ORDER BY data_consegna, sede_consegna;
+QUERY_SUMMARY_VIAGGI = """
+    WITH viaggi AS (
+        SELECT ROW_NUMBER() OVER (PARTITION BY targa ORDER BY data_consegna, sede_consegna) AS id,
+            targa,
+            data_consegna, 
+            sede_consegna
+        FROM vanaheim.consegne
+        WHERE EXTRACT(YEAR FROM data_documento) = 2024
+    ) 
+    SELECT v1.data_consegna data_es745wh,
+        v1.sede_consegna sede_es745wh,
+        v2.sede_consegna sede_fc065zw,
+        v2.data_consegna data_fc065zw
+    FROM (
+        SELECT id,
+            data_consegna,
+            sede_consegna
+        FROM viaggi
+        WHERE targa = 'ES745WH'
+    ) v1
+        FULL JOIN (
+            SELECT id, 
+                data_consegna,
+                sede_consegna
+            FROM viaggi
+            WHERE targa = 'FC065ZW'
+        ) v2 ON v1.id = v2.id
+    ORDER BY COALESCE(v1.id, v2.id);
 """
+
+DEFAULT_FONT = 'Arial'
+FORMATS = {
+    str: '@',
+    int: '#,##0',
+    date: 'dd/mm',
+    'number': '0'
+}
 
 
 def overview_gnr(anno: int = date.today().year, mese: int = date.today().month) -> None:
@@ -41,53 +71,36 @@ def overview_gnr(anno: int = date.today().year, mese: int = date.today().month) 
     logger = logger_ini(PATH_LOG, 'overview_doc')
     cursor = sqlmng.conx_ini()
 
-    consegne = sqlmng.conx_read(cursor, QUERY_OVERVIEW_DATA, (anno, mese))
-    if not consegne.rowcount:
+    consegne = sqlmng.conx_read(cursor, QUERY_OVERVIEW_DATA, (anno, mese)).fetchall()
+    if not consegne:
         logger.warning(f'no record founded in {anno}/{mese:0>2}... skipping overview!')
         return
 
     wb = openpyxl.load_workbook(f'{PATH_SCHEME}/consegne.xlsx')
     ws = wb['consegne']
-    for row_num, row in enumerate(consegne.fetchall(), start=2):
-        # FIXME: make for in for, remove hard code
-        ws.cell(row=row_num, column=1).value = row.numero_documento
-        ws.cell(row=row_num, column=1).font = Font(name='Arial')
-        ws.cell(row=row_num, column=1).number_format = '0'
-
-        ws.cell(row=row_num, column=2).value = row.data_documento
-        ws.cell(row=row_num, column=2).font = Font(name='Arial')
-        ws.cell(row=row_num, column=2).number_format = 'dd/mm'
-
-        ws.cell(row=row_num, column=3).value = row.ragione_sociale
-        ws.cell(row=row_num, column=3).font = Font(name='Arial')
-        ws.cell(row=row_num, column=3).number_format = '@'
-
-        ws.cell(row=row_num, column=4).value = row.sede_consegna
-        ws.cell(row=row_num, column=4).font = Font(name='Arial')
-        ws.cell(row=row_num, column=4).number_format = '@'
-
-        ws.cell(row=row_num, column=5).value = row.quantita
-        ws.cell(row=row_num, column=5).font = Font(name='Arial')
-        ws.cell(row=row_num, column=5).number_format = '#,##0'
-
-        ws.cell(row=row_num, column=6).value = row.data_consegna
-        ws.cell(row=row_num, column=6).font = Font(name='Arial')
-        ws.cell(row=row_num, column=6).number_format = 'dd/mm'
-
-        ws.cell(row=row_num, column=7).value = row.targa
-        ws.cell(row=row_num, column=7).font = Font(name='Arial')
-        ws.cell(row=row_num, column=7).number_format = '@'
+    for row_num, row in enumerate(consegne, start=2):
+        for col_num, col in enumerate(row, start=1):
+            ws.cell(row=row_num, column=col_num).value = col
+            ws.cell(row=row_num, column=col_num).font = Font(name=DEFAULT_FONT)
+            ws.cell(row=row_num, column=col_num).number_format = FORMATS[type(col)] if col_num != 1 else FORMATS['number']
 
     wsc = wb['cifre']
     wsl = wb['litri']
-    for row_num, day in enumerate([date for date in Calendar().itermonthdates(anno, mese) if date.month == mese], start=3):
+    wsc.cell(row=1, column=1).value = date.today().year
+    wsc.cell(row=1, column=1).font = Font(name=DEFAULT_FONT)
+    wsc.cell(row=1, column=1).number_format = FORMATS['number']
+    wsl.cell(row=1, column=1).value = date.today().year
+    wsl.cell(row=1, column=1).font = Font(name=DEFAULT_FONT)
+    wsl.cell(row=1, column=1).number_format = FORMATS['number']
+
+    for row_num, day in enumerate([d for d in Calendar().itermonthdates(anno, mese) if d.month == mese], start=3):
         wsc.cell(row=row_num, column=1).value = day
-        wsc.cell(row=row_num, column=1).font = Font(name='Arial')
-        wsc.cell(row=row_num, column=1).number_format = 'dd/mm'
+        wsc.cell(row=row_num, column=1).font = Font(name=DEFAULT_FONT)
+        wsc.cell(row=row_num, column=1).number_format = FORMATS[date]
 
         wsl.cell(row=row_num, column=1).value = day
-        wsl.cell(row=row_num, column=1).font = Font(name='Arial')
-        wsl.cell(row=row_num, column=1).number_format = 'dd/mm'
+        wsl.cell(row=row_num, column=1).font = Font(name=DEFAULT_FONT)
+        wsl.cell(row=row_num, column=1).number_format = FORMATS[date]
     # TODO: remove extra days (31/06, 30/02, ...)
 
     logger.info('saving overview for {0}... [{0}.xlsx]'.format(f'{anno}_{mese:0>2}'))
@@ -95,10 +108,11 @@ def overview_gnr(anno: int = date.today().year, mese: int = date.today().month) 
     cursor.close()
 
 
-def travels_summary(anno: int = date.today().year) -> None:
+def summary_viaggi(anno: int = date.today().year) -> None:
     # TODO: save summary of travels (data_consegna, sede_consegna)
     pass
 
 
 if __name__ == '__main__':
+    # TODO: make mese optional in overview_gnr()
     overview_gnr()
