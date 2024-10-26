c81572348ce9ad084f88df9a7c8654986c64eeb5
Alex Occelli <occelli.alex@gmail.com>
2024-10-26 14:27:33 +0200

    vanaheim.sh v1.0.1 - bug fixing

--- scheme/vanaheim.sh
+++ scheme/vanaheim.sh
@@ -1,5 +1,5 @@
 # run recording
-python -c "import sys; sys.path.extend(['c:/source/vanaheim', 'c:/source/vanaheim/src', 'c:/source/vanaheim/src'])" && python c:/source/vanaheim/src/recording_doc.py
+python -c "import sys; sys.path.extend(['c:/source/vanaheim', 'c:/source/vanaheim/share', 'c:/source/vanaheim/src'])" && python c:/source/vanaheim/src/recording_doc.py
 
 # make schema backup
 pg_dump -U postgres -h 127.0.0.1 -p 5432 -d postgres -n vanaheim -F c -f "c:/source/vanaheim/scheme/reserves/vanaheim_$(date -f 'yyyy_MM_dd').dump"
